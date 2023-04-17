9c47645abe1f1c981a9e057e74b45e4df38e1f4c
joaovictorjs <skai7ami@gmail.com>
2023-04-17 09:51:58 -0300

    testes em jsonic

--- test/jsonic_test.dart
+++ test/jsonic_test.dart
@@ -10,7 +10,9 @@ void main() {
     Jsonic jsonic = Jsonic();
 
     test("should add a field", () {
-      jsonic.add<String>(JsonicField(mapping: "name")).add<int>(JsonicField(mapping: "age"));
+      jsonic
+          .add<String>(JsonicField(mapping: "name"))
+          .add<int>(JsonicField(mapping: "age"));
 
       expect(jsonic.fields.length, 2);
       expect(jsonic.fields[0].mapping, "name");
@@ -24,14 +26,45 @@ void main() {
         .add<int>(JsonicField(mapping: "age"))
         .add<String>(JsonicField(mapping: "status", fallback: "ACTIVE"));
 
-    String encoded = jsonEncode({"name": "person's name", "age": 123});
+    var raw = {"name": "person's name", "age": 123};
+    String encoded = jsonEncode(raw);
 
     test("should throws a MalformedJson", () {
       try {
-        jsonic.decodeJson(jsonEncode(""));
+        jsonic.decode(jsonEncode(""));
       } on Error catch (e) {
         expect(e, isA<MalformedJson>());
       }
+    });
+
+    test("should returns a map with status = ACTIVE", () {
+      var decoded = jsonic.decodeToMap(encoded);
+
+      expect(decoded["status"], "ACTIVE");
+      expect(decoded["name"], raw["name"]);
+      expect(decoded["age"], raw["age"]);
+    });
+
+    test("should throws an InvalidNullValue", () {
+      jsonic.add<Map>(JsonicField(mapping: "parents"));
+
+      try {
+        jsonic.decode(encoded);
+      } on Error catch (e) {
+        expect(e, isA<InvalidNullValue>());
+      }
+
+      jsonic.fields.removeLast();
+    });
+
+    test("should not throws an InvalidNullValue", () {
+      jsonic.add<String>(JsonicField(mapping: "email", nullable: true));
+
+      var decoded = jsonic.decodeToMap(encoded);
+
+      expect(decoded["status"], "ACTIVE");
+      expect(decoded["name"], raw["name"]);
+      expect(decoded["age"], raw["age"]);
     });
   });
 }
